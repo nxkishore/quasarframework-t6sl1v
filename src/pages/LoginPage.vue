f83e34c1ab9e684a8f481846e66ea943b5b4d87f
nxkishore <xavier.narisetti@gmail.com>
2022-08-07 19:20:41 -0400

    2 files modified

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,6 +1,23 @@
 <template>
-  <q-page padding>
-    Login Page
+  <q-page class="flex flex-center" padding>
+    <div class="full.width" style="max-width: 350px;">
+      <h5 class="q-my-md text-center"> 
+        Login 
+      </h5>
+      <q-card>
+        <q-card-section>
+        <q-form>
+
+          <q-input filled label = "email" placeholder="email@example.com" class = "q-mb-md" />
+          <q-input label = "password" type="password" class = "q-mb-sm" />
+          <q-checkbox label ="Remember me" :model-value ="true"/>
+          </q-form>
+        </q-card-section>
+
+        <q-btn label="Login" class="full-width" color="primary"> 
+        </q-btn>
+        </q-card> 
+     </div>
   </q-page>
 </template>
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,14 @@ const routes = [
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
 
+  // /auth/login
+
+  {
+    path: '/auth',
+    component: () => import('layouts/BlankLayout.vue'),
+    children: [{ path: 'Login', component: () => import('pages/LoginPage.vue') }],
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
